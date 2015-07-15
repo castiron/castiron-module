1a989cc789a068edf346ec92e602ec014c435e24
Peter Soots <peter@castironcoding.com>
2015-07-15 13:51:55 -0700

    [FEATURE] Creates ComponentBase to easily add variables to component views.

---
+++ components/ComponentBase.php
@@ -0,0 +1,28 @@
+<?php namespace Castiron\Components;
+
+use Cms\Classes\ComponentBase as Base;
+
+abstract class ComponentBase extends Base
+{
+    protected $vars = [];
+    protected $varsConf = [];
+
+    public function onRun()
+    {
+        $this->loadVariables();
+    }
+
+    /**
+     * This function adds variables to the twig template context. Since
+     * it uses the ComponentProperty, those functions won't be invoked
+     * until the variable is used in the template.
+     */
+    protected function loadVariables()
+    {
+        foreach ($this->vars as $key) {
+            if (isset($this->controller->vars[$key])) continue;
+            $conf = isset($this->varsConf[$key]) ? $this->varsConf[$key] : null;
+            $this->controller->vars[$key] = new ComponentProperty($this, $key, $conf);
+        }
+    }
+}
