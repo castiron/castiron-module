4ee013146248d8e44bb9231e6004d815801e2fdd
Peter Soots <peter@castironcoding.com>
2015-08-19 10:52:28 -0700

    [BUGFIX] Function shortcuts were not bound to their component.
    When extending ComponentBase and adding to the $funcs[] array so that your functions are easily available in the twig templates, you expect your function to only be called on your component. However, twig stores all functions in an array by name, so two functions with the same name will not resolve properly. To get around this, we're using the twig closure to get the current __SELF__ object and invoking the function on that. Basically, our component functions are no longer going to be global.
    
    One important thing to note is that you should not be able to override a non-component function. I'm not sure how to enforce this yet.

--- components/ComponentBase.php
+++ components/ComponentBase.php
@@ -25,30 +25,76 @@ abstract class ComponentBase extends Base
     protected function loadContext()
     {
         foreach ($this->funcs as $key) {
-            $this->addViewFunction($key, [$this, $key]);
+            $this->addViewFunction($key);
         }
         foreach ($this->funcsNotCached as $key) {
-            $this->addViewFunction($key, [$this, $key], false);
+            $this->addViewFunction($key, false);
         }
     }
 
     /**
      * Adds a function to the twig context.
      *
-     * @param string $name
-     * @param callable $callable
+     * @param string $funcName
      * @param bool|true $once If true, wraps the function in a closure so the callable is only invoked once.
      */
-    protected function addViewFunction($name, $callable, $once = true)
+    protected function addViewFunction($funcName, $once = true)
     {
+
         if ($once) {
-            $callable = function() use ($callable) {
-                static $result = null;
-                if ($result) return $result;
-                $result = call_user_func($callable);
+            $realCallable = function($context) use ($funcName) {
+                static $results = [];
+
+                // use the __SELF__ object from the context so that
+                // our callable only applies to the relevant component
+                $obj = $context['__SELF__'];
+                if (!$obj) return;
+
+                // return early if we have a cached result
+                foreach ($results as $result) {
+                    if ($result['obj'] === $obj) {
+                        return $result['return'];
+                    }
+                }
+
+                // call the function with the given args
+                $args = func_get_args();
+                array_shift($args);
+                $result = call_user_func_array([$obj, $funcName], $args);
+
+                // save the result for later
+                $results[] = [
+                    'obj' => $obj,
+                    'return' => $result,
+                ];
+
+
                 return $result;
             };
+        } else {
+            $realCallable = function($context) use ($funcName) {
+                // use the __SELF__ object from the context so that
+                // our callable only applies to the relevant component
+                $obj = $context['__SELF__'];
+                if (!$obj) return;
+
+                // call the function with the given args
+                $args = func_get_args();
+                array_shift($args);
+                return call_user_func_array([$obj, $funcName], $args);
+            };
         }
-        $this->twig->addFunction($name, new \Twig_SimpleFunction($name, $callable));
+
+
+
+        $funcObj = new \Twig_SimpleFunction($funcName, $realCallable, [
+            'needs_context' => true,
+        ]);
+        $this->twig->addFunction($funcName, $funcObj);
+
+
     }
+
+
+
 }
